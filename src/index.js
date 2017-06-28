7b448ebfbf766e1e1b92bf1be28cda525341d996
Eunbi Go <eunbigo91@gmail.com>
2017-06-28 14:50:46 -0700

    Add: input tag and js files to build a youtube video search web app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAd2HWNEr3C9X--wOTcw9Q1wDJloQXZuxU';
+
 // Create a new component. This component should produce
 // some HTML
 
 // const App = function() {
 const App = () => {
-    return <div> Hi! </div>;
+    return (
+		<div> 
+	    	<SearchBar />
+	    </div>
+    );
 }
 
 // Take this component's generated HTML and put it
