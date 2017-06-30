b7f437b02899c8d8a45ef0f2218f62f1f72f7204
Eunbi Go <eunbigo91@gmail.com>
2017-06-30 13:42:20 -0700

    Add: video lists from youtube

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAd2HWNEr3C9X--wOTcw9Q1wDJloQXZuxU';
 
@@ -12,8 +13,9 @@ class App extends Component {
 
 		this.state = { videos: [] };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos: videos });
+		YTSearch({key: API_KEY, term: 'troll'}, (videos) => {
+			this.setState({ videos });
+			//this.setState({ videos: videos });
 		});
 	}
 
@@ -21,6 +23,7 @@ class App extends Component {
 		return (
 			<div> 
 		    	<SearchBar />
+		    	<VideoList videos={this.state.videos} />
 		    </div>
 	    );
 	}
