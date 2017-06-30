0d8dd0cfd3c4b3bf82fa45ce3757a9f88bddf755
Eunbi Go <eunbigo91@gmail.com>
2017-06-30 12:37:34 -0700

    Add: 'value' in input tag, YTSearch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAd2HWNEr3C9X--wOTcw9Q1wDJloQXZuxU';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
+
 
 // Create a new component. This component should produce
 // some HTML
